05388d78a75aadbe8f6857d6bc7d1353e4db3e6a
Gabor Marton <gabor.marton@prezi.com>
2016-01-09 16:41:12 +0100

    Move out dist decl from inner loop

--- race_test/ExecuteInLoop.hpp
+++ race_test/ExecuteInLoop.hpp
@@ -5,39 +5,41 @@
 namespace detail {
 
 struct RandomWait {
-	std::mt19937 random_engine{ std::random_device{}() };
-	void operator()() {
-		static const constexpr std::size_t MAX = 100000;
-		std::uniform_int_distribution<std::size_t> dist(0, MAX);
-		std::size_t N = dist(random_engine);
-		for (std::size_t i = 0; i < N; ++i) {
-			// http://stackoverflow.com/questions/7083482/how-to-prevent-gcc-from-optimizing-out-a-busy-wait-loop
-			__asm__("");
-		}
-	}
+
+    std::mt19937 random_engine{std::random_device{}()};
+    static const constexpr std::size_t MAX = 100000;
+    std::uniform_int_distribution<std::size_t> dist{0, MAX};
+
+    void operator()() {
+        std::size_t N = dist(random_engine);
+        for (std::size_t i = 0; i < N; ++i) {
+            // http://stackoverflow.com/questions/7083482/how-to-prevent-gcc-from-optimizing-out-a-busy-wait-loop
+            __asm__("");
+        }
+    }
 };
 
 template <std::size_t N, typename F>
 struct InLoopExecutor {
-	InLoopExecutor(F f) : f(f) {}
-	RandomWait random_wait{};
-	void operator()() {
-		for (std::size_t i = 0; i < N; ++i) {
-			f();
-			random_wait();
-		}
-	}
-	F f;
+    InLoopExecutor(F f) : f(f) {}
+    RandomWait random_wait{};
+    void operator()() {
+        for (std::size_t i = 0; i < N; ++i) {
+            f();
+            random_wait();
+        }
+    }
+    F f;
 };
 
 } // namespace detail
 
 template <std::size_t N, typename F>
 auto makeInLoopExecutor(F f) {
-	return detail::InLoopExecutor<N, F>{f};
+    return detail::InLoopExecutor<N, F>{f};
 }
 
 template <std::size_t N, typename F>
 void executeInLoop(F f) {
-	detail::InLoopExecutor<N, F>{f} ();
+    detail::InLoopExecutor<N, F>{f}();
 }
