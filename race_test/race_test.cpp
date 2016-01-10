de6b6a5ee8b9bf075f1df8c227b27871c2cc6418
Gabor Marton <gabor.marton@prezi.com>
2016-01-10 15:10:44 +0100

    Add test_copyctor_write

--- race_test/race_test.cpp
+++ race_test/race_test.cpp
@@ -75,9 +75,30 @@ void test_assign_write() {
     t2.join();
 }
 
+void test_copyctor_write() {
+    auto p = versioned_shared_ptr<int>{};
+
+    std::thread t2{[&p]() {
+        executeInLoop<10000>([&p]() {
+			auto p2 = p;
+        });
+    }};
+
+    std::thread t1{[&p]() {
+        executeInLoop<10000>([&p]() {
+            auto new_ = std::make_shared<int>(42);
+            p.write(new_);
+        });
+    }};
+
+    t1.join();
+    t2.join();
+}
+
 int main() {
     test_read_write();
     test_write_write();
     test_read_read();
 	test_assign_write();
+	test_copyctor_write();
 }
