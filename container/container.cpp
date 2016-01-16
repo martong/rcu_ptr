5f9b761fa7a584e0559244f6d591a93c2543dc01
Gabor Marton <gabor.marton@prezi.com>
2016-01-16 19:03:42 +0100

    Add container dir for text

---
+++ container/container.cpp
@@ -0,0 +1,132 @@
+#include <vector>
+#include <numeric>
+#include <mutex>
+#include <memory>
+#include "../versioned_shared_ptr.hpp"
+#include "../race_test/ExecuteInLoop.hpp"
+#include <thread>
+
+#define ASSERT(CONDITION)                                                      \
+  do                                                                           \
+    if (!(CONDITION)) {                                                        \
+      printf("Assertion failure %s:%d ASSERT(%s)\n", __FILE__, __LINE__,       \
+             #CONDITION);                                                      \
+      abort();                                                                 \
+    }                                                                          \
+  while (0)
+
+
+//class X {
+    //std::vector<int> v;
+    //mutable std::mutex m;
+//public:
+    //int sum() const { // read operation
+        //std::lock_guard<std::mutex> lock{m};
+        //return std::accumulate(v.begin(), v.end(), 0);
+    //}
+    //void add(int i) { // write operation
+        //std::lock_guard<std::mutex> lock{m};
+        //v.push_back(i);
+    //}
+//};
+
+//class X {
+    //std::shared_ptr<std::vector<int>> v;
+    //mutable std::mutex m;
+//public:
+    //X() : v(std::make_shared<std::vector<int>>()) {}
+    //int sum() const { // read operation
+        //std::shared_ptr<std::vector<int>> local_copy;
+        //{
+            //std::lock_guard<std::mutex> lock{m};
+            //local_copy = v;
+        //}
+        //return std::accumulate(local_copy->begin(), local_copy->end(), 0);
+    //}
+    //void add(int i) { // write operation
+        //std::shared_ptr<std::vector<int>> local_copy;
+        //{
+            //std::lock_guard<std::mutex> lock{m};
+            //local_copy = v;
+        //}
+        //auto local_deep_copy = std::make_shared<std::vector<int>>(*local_copy);
+        //local_deep_copy->push_back(i);
+        //{
+            //std::lock_guard<std::mutex> lock{m};
+            //v = local_deep_copy;
+        //}
+    //}
+//};
+
+//class X {
+    //std::shared_ptr<std::vector<int>> v;
+//public:
+    //X() : v(std::make_shared<std::vector<int>>()) {}
+    //int sum() const { // read operation
+        //auto local_copy = std::atomic_load(&v);
+        //return std::accumulate(local_copy->begin(), local_copy->end(), 0);
+    //}
+    //void add(int i) { // write operation
+        //auto local_copy = std::atomic_load(&v);
+        //auto exchange_result = false;
+        //while (!exchange_result) {
+            //// we need a deep copy
+            //auto local_deep_copy = std::make_shared<std::vector<int>>(
+                    //*local_copy);
+            //local_deep_copy->push_back(i);
+            //exchange_result =
+                //std::atomic_compare_exchange_strong(&v, &local_copy,
+                                                    //local_deep_copy);
+        //}
+    //}
+//};
+
+class X {
+    versioned_shared_ptr<std::vector<int>> v;
+
+public:
+    X() { v.overwrite(std::make_shared<std::vector<int>>()); }
+    int sum() const { // read operation
+        std::shared_ptr<const std::vector<int>> local_copy = v.read();
+        return std::accumulate(local_copy->begin(), local_copy->end(), 0);
+    }
+    void add(int i) { // write operation
+        v.update([i](const std::vector<int>& v) {
+            auto new_ = v;
+            new_.push_back(i);
+            return new_;
+        });
+    }
+};
+
+void test_sum_add() {
+    X x{};
+
+    int sum = 0;
+    std::thread t2{[&x, &sum]() {
+        executeInLoop<1000>([&x, &sum]() {
+            sum = x.sum();
+        });
+    }};
+
+    std::thread t1{[&x]() {
+        executeInLoop<1000>([&x]() {
+            x.add(3);
+        });
+    }};
+
+    std::thread t3{[&x]() {
+        executeInLoop<1000>([&x]() {
+            x.add(4);
+        });
+    }};
+
+    t1.join();
+    t2.join();
+    t3.join();
+    ASSERT(x.sum() == 7000);
+}
+
+int main() {
+    test_sum_add();
+}
